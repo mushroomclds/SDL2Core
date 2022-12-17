13f4b2345cb7583950babba96419d46511141655
mushroomclds <aldaymario25@gmail.com>
2022-12-16 20:09:12 -0600

    added Boost libaries

--- include/DEFINITIONS.hpp
+++ include/DEFINITIONS.hpp
@@ -1,4 +1,36 @@
 #pragma once
+#include <string>
+#include <boost/log/core.hpp>
+#include <boost/log/trivial.hpp>
+#include <boost/log/expressions.hpp>
+#include <boost/log/sinks/text_file_backend.hpp>
+#include <boost/log/utility/setup/file.hpp>
+#include <boost/log/utility/setup/common_attributes.hpp>
+#include <boost/log/sources/severity_logger.hpp>
+#include <boost/log/sources/record_ostream.hpp>
+#include <iomanip>
+
+#define LFC1_LOG_TRACE(logger)
+
+#define __FILENAME__ (strrchr(__FILE__, '\\') ? strrchr(__FILE__, '\\') + 1 : __FILE__)
+#define LOG               \
+  BOOST_LOG_TRIVIAL(info) \
+      << "[" << __FILENAME__ << " | Line: " << __LINE__ << "]"  //<< std::setw(30) << std::left
+
+#define FRAMERATE 75
 
 #define VIDEOMODE_WIDTH 800
 #define VIDEOMODE_HEIGHT 600
+#define HALF_WIN_SIZE 2
+
+#define GRAVITY 1.f
+#define MOVE_RIGHT 0.01f
+#define MOVE_LEFT 0.01f
+#define JUMP 10.f
+#define DOUBLE_JUMP_MAX 15
+#define MOVE_DOWN 0.1f
+#define MOVE_SPEED_ALL 5.f
+
+#define PLAYER_SIZE 60.f
+#define WIDTH_OFFSET 10
+#define PLATFORM_WIDTH 10
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,26 @@
 #include <SDL2/SDL.h>
 #include <iostream>
+#include "../include/DEFINITIONS.hpp"
+
+namespace logging  = boost::log;
+namespace keywords = boost::log::keywords;
+void InitBoost() {
+  keywords::auto_flush = true;
+  boost::log::add_common_attributes();
+  logging::add_file_log(keywords::file_name = "../default_%N.log",
+                        keywords::format    = "%Message% ");  //%TimeStamp%
+  logging::core::get()->set_filter(logging::trivial::severity >= logging::trivial::info);
+}
 
 int main(int argv, char** args) {
+
+  InitBoost();  //intialize boost logging
+  LOG << "Boost Logging Successful";
+
   SDL_Init(SDL_INIT_EVERYTHING);
 
-  SDL_Window* window     = SDL_CreateWindow("Hello SDL",
-                                        SDL_WINDOWPOS_CENTERED,
-                                        SDL_WINDOWPOS_CENTERED,
-                                        800,
-                                        600,
-                                        0);
+  SDL_Window* window =
+      SDL_CreateWindow("Hello SDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, 0);
   SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
 
   bool isRunning = true;
